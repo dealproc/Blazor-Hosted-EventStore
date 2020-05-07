58fa4b8a835ca650c2456262f3ac2fe78658d147
Richard Bennett <inbox@richardbennett.dev>
2020-05-07 06:03:27 -0400

    Basic system is together, but service is still failing to start.
    Seems to be an issue with Newtonsoft.Json, that for some reason, there is an inability to load the file into mem.  Need to research.

--- source/app/Program.cs
+++ source/app/Program.cs
@@ -15,33 +15,19 @@ namespace app
     {
         public static async Task Main(string[] args)
         {
-            // IEventStoreConnection _connection;
-            // EventStoreConnection.Create(new Uri(""), "");
-
-            // var nodeBuilder = EmbeddedVNodeBuilder
-            //     .AsSingleNode()
-            //     .StartStandardProjections()
-            //     .RunProjections(ProjectionType.All)    
-            //     .RunInMemory()
-            //     .WithInternalHttpOn(new IPEndPoint(IPAddress.Loopback, 1112))
-            //     .WithExternalHttpOn(new IPEndPoint(IPAddress.Loopback, 1113))
-            //     .WithExternalTcpOn(new IPEndPoint(IPAddress.Loopback, 1114))
-            //     .WithInternalTcpOn(new IPEndPoint(IPAddress.Loopback, 1115))
-            //     .DisableDnsDiscovery()
-            //     .WithGossipSeeds(new IPEndPoint[]
-            //     {
-            //         new IPEndPoint(IPAddress.Loopback, 2112),
-            //         new IPEndPoint(IPAddress.Loopback, 3112)
-            //     });                    
-            // var _node = nodeBuilder.Build();
-            // _node.StartAsync(true).Wait(TimeSpan.FromSeconds(30));
-            
-            // _connection = EmbeddedEventStoreConnection.Create(_node);
-
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            // event store
+            builder.Services.AddSingleton<IEventStoreConnection>((ctx) => {
+                // var options = ctx.GetService<IOptions<ConnectionStringsConfiguration>>();
+                // var config = options.Value;
+                //var conn = EventStoreConnection.Create(config.EventStore, $"processing-api-{System.Environment.MachineName}");
+                var conn = EventStoreConnection.Create("ConnectTo=tcp://admin:changeit@localhost:1113; HeartBeatTimeout=300000", $"processing-api-{System.Environment.MachineName}");
+                conn.ConnectAsync().Wait(); //TODO: Is there a better way than saying "Wait()"?
+                return conn;
+            });
 
             await builder.Build().RunAsync();
         }
--- source/host/Program.cs
+++ source/host/Program.cs
@@ -1,28 +1,52 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ElectronNET.API;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.Net;
 
-namespace host
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+using ElectronNET.API;
+
+using EventStore.ClientAPI.Embedded;
+using EventStore.Common.Options;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace host {
+    public class Program {
+        public static void Main(string[] args) {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Verbose()
+                .CreateLogger();
+
+
+            var nodeBuilder = EmbeddedVNodeBuilder
+                .AsSingleNode()
+                .StartStandardProjections()
+                .RunProjections(ProjectionType.All)
+                .RunInMemory()
+                .WithInternalHttpOn(new IPEndPoint(IPAddress.Loopback, 1112))
+                .WithExternalHttpOn(new IPEndPoint(IPAddress.Loopback, 1113))
+                .WithExternalTcpOn(new IPEndPoint(IPAddress.Loopback, 1114))
+                .WithInternalTcpOn(new IPEndPoint(IPAddress.Loopback, 1115))
+                .DisableDnsDiscovery()
+                .WithGossipSeeds(new IPEndPoint[] {
+                    new IPEndPoint(IPAddress.Loopback, 2112),
+                        new IPEndPoint(IPAddress.Loopback, 3112)
+                });
+            var _node = nodeBuilder.Build();
+            _node.StartAsync(true).Wait(TimeSpan.FromSeconds(30));
+
+            Log.Information("Hello world!");
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseElectron(args);
-                    webBuilder.UseStartup<Startup>();
-                });
+            .ConfigureWebHostDefaults(webBuilder => {
+                webBuilder.UseElectron(args);
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
