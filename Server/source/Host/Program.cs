e25b0fe10cacdffdfa7cff190fe50c8e323ef6c3
Richard Bennett <inbox@richardbennett.dev>
2020-05-13 12:25:16 -0400

    Adds VNode setup for local EventStore instance.

--- Server/source/Host/Program.cs
+++ Server/source/Host/Program.cs
@@ -1,15 +1,30 @@
+using ElectronNET.API;
+
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
+using Serilog;
 
 namespace Host {
     public class Program {
         public static void Main(string[] args) {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .Enrich.FromLogContext()
+                .MinimumLevel.Verbose()
+                .CreateLogger();
+
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services => {
+                services.AddHostedService<Services.EmbeddedEventStoreService>();
+            })
             .ConfigureWebHostDefaults(webBuilder => {
+                webBuilder.UseElectron(args);
                 webBuilder.UseStartup<Startup>();
             });
     }
