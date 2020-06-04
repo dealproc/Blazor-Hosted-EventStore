683be11fb6381b7f27e206a23e9a1a7813933eab
Richard Bennett <inbox@richardbennett.dev>
2020-06-04 16:25:22 -0400

    Adds multiple counters for a single page.

--- Server/source/Host/Pages/Index.razor.cs
+++ Server/source/Host/Pages/Index.razor.cs
@@ -1,65 +1,114 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using System.Threading.Tasks;
 
 using EventStore.ClientAPI;
-using EventStore.ClientAPI.SystemData;
 
 using Microsoft.AspNetCore.Components;
 
+using Serilog;
+using ILogger = Serilog.ILogger;
+
 namespace Host.Pages {
-    public class IndexViewModel : ComponentBase {
+    public class IndexViewModel : ComponentBase, IDisposable {
+        static ILogger s_log = Log.Logger.ForContext<IndexViewModel>();
         const string _subscriptionStream = "$et-announcement-message";
         const string _channel = "announcements-channel";
         const string _eventName = "announcement-message";
 
+        private int _nextCounterNumber = 0;
+        private EventStorePersistentSubscriptionBase _subscription;
+
         [Inject]
         protected IEventStoreConnection Connection { get; set; }
-        protected long EventsReceived { get; private set; }
-        protected EventStorePersistentSubscriptionBase ClickSubscription { get; private set; }
+        protected List<Counter> Counters { get; private set; } = new List<Counter>();
+
+        protected async Task AddCounter() {
+            _nextCounterNumber++;
+            s_log.Information("Adding a counter.");
+
+            await InvokeAsync(() => {
+                Counters.Add(new Counter {
+                    CounterId = _nextCounterNumber,
+                        Label = $"Counter {_nextCounterNumber}",
+                        NumberOfClicks = 0
+                });
+                StateHasChanged();
+                s_log.Information("Counter added.");
+            });
+        }
+
+        protected async Task ClickCounter(Counter counter) {
+            s_log.Information("Counter clicked.");
+            var msg = new CounterClicked {
+                CounterId = counter.CounterId
+            };
+            var data = new EventData(
+                Guid.NewGuid(),
+                "counter-clicked",
+                true,
+                JsonSerializer.SerializeToUtf8Bytes(msg),
+                new byte[0]
+            );
+            await Connection.AppendToStreamAsync(_subscriptionStream, ExpectedVersion.Any, new [] { data }, default);
+        }
 
         protected override async Task OnInitializedAsync() {
             await base.OnInitializedAsync();
-            
-            var subscriptionSettings = PersistentSubscriptionSettings.Create()
+            var subSettings = PersistentSubscriptionSettings.Create()
                 .ResolveLinkTos()
                 .StartFromBeginning()
                 .Build();
 
             try {
-                await Connection.CreatePersistentSubscriptionAsync(_subscriptionStream, _channel,
-                    subscriptionSettings, default); //, new UserCredentials("admin", "changeit"));
+                await Connection.CreatePersistentSubscriptionAsync(_subscriptionStream,
+                    _channel,
+                    subSettings,
+                    default);
             } catch (Exception) {
-                await Connection.UpdatePersistentSubscriptionAsync(_subscriptionStream, _channel,
-                    subscriptionSettings, default); //, new UserCredentials("admin", "changeit"));
+                await Connection.UpdatePersistentSubscriptionAsync(_subscriptionStream,
+                    _channel,
+                    subSettings,
+                    default);
             }
 
-            ClickSubscription = await Connection.ConnectToPersistentSubscriptionAsync(
+            _subscription = await Connection.ConnectToPersistentSubscriptionAsync(
                 stream: _subscriptionStream,
                 groupName: _channel,
                 eventAppeared: async(sub, e, position) => {
+                    s_log.Information("Received counter-click.");
                     await InvokeAsync(() => {
-                        EventsReceived += 1;
+                        s_log.Information("Received counter-click.");
+                        var clickedOn = JsonSerializer.Deserialize<CounterClicked>(new ReadOnlySpan<byte>(e.Event.Data));
+                        var counter = Counters.SingleOrDefault(c => c.CounterId == clickedOn.CounterId);
+                        if (counter != null) {
+                            s_log.Information("Counter entry found.  Adding +1 to value.");
+                            counter.NumberOfClicks += 1;
+                        }
+
                         StateHasChanged();
                     });
                 },
                 subscriptionDropped: (sub, reason, exc) => { },
-                userCredentials : default ); // : new UserCredentials("admin", "changeit"));
+                userCredentials : default
+            );
         }
 
-        protected async Task Send() {
-            var msg = new AnAnnouncement {
-                Title = "Test Message",
-                Message = "Some message to send over to a user."
-            };
-            var data = new EventData(
-                Guid.NewGuid(),
-                _eventName,
-                true,
-                JsonSerializer.SerializeToUtf8Bytes(msg),
-                new byte[0]);
-
-            await Connection.AppendToStreamAsync(_subscriptionStream, ExpectedVersion.Any, new [] { data }, default); //, new UserCredentials("admin", "changeit"));
+        public void Dispose() {
+            _subscription?.Stop(TimeSpan.FromSeconds(30));
+            _subscription = null;
         }
+    }
+
+    public class Counter {
+        public int CounterId { get; set; }
+        public string Label { get; set; }
+        public int NumberOfClicks { get; set; }
+    }
+
+    public struct CounterClicked {
+        public int CounterId { get; set; }
     }
 }
